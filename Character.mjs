d67bf9b415bf7943ae0760ae42c0afd4480fc454
Sergio Galdos <galdos.sergio@outlook.com>
2025-05-08 09:57:05 +0200

    [DEV] UPDATE create characters

--- Character.mjs
+++ Character.mjs
@@ -1,3 +1,5 @@
+import Weapon from "./Weapon.mjs"
+
 export default class Character {
   constructor(name, occupation, gold, level, skills, weapon) {
     this.name = name
@@ -6,5 +8,36 @@ export default class Character {
     this.level = level
     this.skills = skills
     this.weapon = weapon
+  }
+
+  static trainForADay(weaponName, day) {
+
+    let points = 0
+
+    // entrenamiento
+    // calcular puntos de arma
+
+    if (this.occupation === "thug") {
+      points = 10
+    }
+
+    if (this.occupation === "priest") {
+      points = 20
+    }
+
+    if (this.occupation === "peasant") {
+      points = 30
+    }
+
+    const weapon = new Weapon(
+      this.weapon.weaponName,
+      this.weapon.type,
+      this.weapon.pointsForNextSkill, points)
+
+      
+
+    // actualizar datos de personaje
+    // actualizar datos de arma
+
   }
 }
--- index.mjs
+++ index.mjs
@@ -7,16 +7,28 @@ createCharacters()
 
 createDay()
 
-
-
 // create Characters
 function createCharacters() {
+
   let characters = []
+  
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
-    let char = new Character(element.name, element.occupation, element.gold, element.level, element.skills, element.weapons)
+
+    let weapons = []
+
+    for (let j = 0; j < element.weapons.length; j++) {
+      const weapon = element.weapons[j];
+      let weap = new Weapon (weapon.name, weapon.type, weapon.points_for_next_skill, 10)
+      weapons.push(weap)
+    }
+
+    let char = new Character(element.name, element.occupation, element.gold, element.level, element.skills, weapons)
+
     characters.push(char)
   }
+
+  console.log(characters)
 }
 
 // create Day object
