f0599361d716530ac26a6660633e2af100409c18
Sergio Galdos <galdos.sergio@outlook.com>
2025-05-08 08:14:24 +0200

    [DEV] first commit

---
+++ data/data.js
@@ -0,0 +1,222 @@
+[
+  {
+    "name": "Garruk Ironhide",
+    "occupation": "peasant",
+    "gold": 1875,
+    "level": 6,
+    "skills": {
+      "brawl": 21,
+      "melee": 17,
+      "missile": 13
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 2
+      },
+      {
+        "name": "Boar Cleaver",
+        "type": "melee",
+        "points_for_next_skill": 4
+      }
+    ]
+  },
+  {
+    "name": "Thoren Duskblade",
+    "occupation": "thug",
+    "gold": 2660,
+    "level": 9,
+    "skills": {
+      "brawl": 28,
+      "melee": 25,
+      "missile": 11
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 3
+      },
+      {
+        "name": "Iron Shiv",
+        "type": "melee",
+        "points_for_next_skill": 2
+      }
+    ]
+  },
+  {
+    "name": "Sereth Moonwhisper",
+    "occupation": "priest",
+    "gold": 1420,
+    "level": 4,
+    "skills": {
+      "brawl": 15,
+      "melee": 12,
+      "missile": 19
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 1
+      }
+    ]
+  },
+  {
+    "name": "Brynn Oakshield",
+    "occupation": "peasant",
+    "gold": 2045,
+    "level": 7,
+    "skills": {
+      "brawl": 26,
+      "melee": 18,
+      "missile": 10
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 4
+      },
+      {
+        "name": "Pitchfork",
+        "type": "melee",
+        "points_for_next_skill": 3
+      }
+    ]
+  },
+  {
+    "name": "Varek Stonegrip",
+    "occupation": "thug",
+    "gold": 1580,
+    "level": 5,
+    "skills": {
+      "brawl": 20,
+      "melee": 14,
+      "missile": 22
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 2
+      },
+      {
+        "name": "Throwing Knife",
+        "type": "missile",
+        "points_for_next_skill": 5
+      }
+    ]
+  },
+  {
+    "name": "Caldus Emberlight",
+    "occupation": "priest",
+    "gold": 1755,
+    "level": 6,
+    "skills": {
+      "brawl": 11,
+      "melee": 19,
+      "missile": 24
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 1
+      },
+      {
+        "name": "Divine Sling",
+        "type": "missile",
+        "points_for_next_skill": 3
+      }
+    ]
+  },
+  {
+    "name": "Eldon Bramblehorn",
+    "occupation": "peasant",
+    "gold": 1390,
+    "level": 3,
+    "skills": {
+      "brawl": 19,
+      "melee": 16,
+      "missile": 12
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 3
+      }
+    ]
+  },
+  {
+    "name": "Mara Blackthorn",
+    "occupation": "thug",
+    "gold": 2120,
+    "level": 8,
+    "skills": {
+      "brawl": 29,
+      "melee": 27,
+      "missile": 15
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 4
+      },
+      {
+        "name": "Shadow Dagger",
+        "type": "melee",
+        "points_for_next_skill": 2
+      }
+    ]
+  },
+  {
+    "name": "Orlan the Meek",
+    "occupation": "priest",
+    "gold": 1100,
+    "level": 2,
+    "skills": {
+      "brawl": 10,
+      "melee": 11,
+      "missile": 30
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 1
+      },
+      {
+        "name": "Holy Bow",
+        "type": "missile",
+        "points_for_next_skill": 3
+      }
+    ]
+  },
+  {
+    "name": "Ysolde Firebloom",
+    "occupation": "peasant",
+    "gold": 2730,
+    "level": 10,
+    "skills": {
+      "brawl": 25,
+      "melee": 30,
+      "missile": 14
+    },
+    "weapons": [
+      {
+        "name": "Fist",
+        "type": "brawl",
+        "points_for_next_skill": 2
+      },
+      {
+        "name": "Rusty Saber",
+        "type": "melee",
+        "points_for_next_skill": 5
+      }
+    ]
+  }
+]
