f7cf129a4d2bdd9a3d58a9064afe11614838904b
Sergio Galdos <galdos.sergio@outlook.com>
2025-05-08 08:52:57 +0200

    [DEV] ADD createCharacters

--- index.mjs
+++ index.mjs
@@ -1,3 +1,16 @@
 import data from "./data/data.mjs";
+import Character from "./Character.mjs";
+import Weapon from "./Weapon.mjs";
+import Day from "./Day.mjs";
 
-console.log(data)
+createCharacters()
+
+// crear characters
+function createCharacters() {
+  let characters = []
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    let char = new Character(element.name, element.occupation, element.gold, element.level, element.skills, element.weapons)
+    characters.push(char)
+  }
+}
